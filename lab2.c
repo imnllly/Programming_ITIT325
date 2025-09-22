12303e66bfab2f60cf6fbeff268fbce4b3e93dcf
imnllly <143996748+imnllly@users.noreply.github.com>
2025-09-22 13:33:25 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,16 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <math.h>
+#include <math.h> 
 
 int main() {
-	float x, y, t;
+	float x, y, w;
 	printf("Enter x:"); scanf("%f", &x);
-	if (x > 0) {y = powf(1 / tanf(x), 2);}
-	else if (x == 1 || x == 2) {y = log10f(x) / log10f(3);}
-	else {y = powf(fabs(x), x);}
-	if ((powf(sinf(y), x) == 0) || (sinf(y) < 0)) {printf("Error!");}
-	else { t = (exp(2 * x + y)) / powf(sinf(y), x); }
-	printf("t=%f", t);
+	if (x >= 3) { y = log10f(x) / log10f(2); }
+		else if (x == 2 || x == 1) { y = powf(sinf(x), 2); }
+			else { y = powf(cosf(x), 3); }
+	if (x < 0 || fabs(2 * x + 0.8) == 0) { printf("Error!"); }
+	else {
+		w = exp(3 * y) / fabs(2 * x + 0.8);
+		printf("w=%f", w);
+	}
 	return 0;
 }
