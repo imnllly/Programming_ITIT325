e077956d7e69c1c964e836f228951c7a72be8169
imnllly <143996748+imnllly@users.noreply.github.com>
2025-11-29 23:19:27 +0300

    Update lab6_2.c

--- lab6_2.c
+++ lab6_2.c
@@ -15,13 +15,13 @@ int main() {
     if (size > MAX_SIZE) { size = MAX_SIZE; }
     for (int i = 0; i < size; i++) {
         Yk[i] = (rand() % 201 - 100) + (rand() % 10) / 10.0;
-        printf("%f \t", *ptr); // Указатель вместо Xn[i]
-        if (*ptr > 0) { // Указатель вместо Xn[i]
+        printf("%f \t", *ptr); // Указатель вместо Yk[i]
+        if (*ptr > 0) { // Указатель вместо Yk[i]
             k++;
-            if (k == 2) { s_pos = *ptr; } // Указатель вместо Xn[i]
+            if (k == 2) { s_pos = *ptr; } // Указатель вместо Yk[i]
             else if (k > 2) { break; }
         }
-        if (fabsf(*ptr - roundf(*ptr)) < 0.0001f) { n++; } // Указатель вместо Xn[i]
+        if (fabsf(*ptr - roundf(*ptr)) < 0.0001f) { n++; } // Указатель вместо Yk[i]
         ptr++; // Увеличение указателя на 1
     }
     ptr = Yk; //
@@ -29,7 +29,7 @@ int main() {
     else {
         printf("\nInteger elements: \t");
         for (int i = 0; i < size; i++) {
-            if (fabsf(*ptr - roundf(*ptr)) < 0.0001f) { printf("%f \t", *ptr); } // Указатель вместо Xn[i]
+            if (fabsf(*ptr - roundf(*ptr)) < 0.0001f) { printf("%f \t", *ptr); } // Указатель вместо Yk[i]
             ptr++; // Увеличение указателя на 1
         }
     }
