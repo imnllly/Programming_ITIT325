52cff64d3e3309c1c0c14220a6bd554b08f8f2e6
imnllly <143996748+imnllly@users.noreply.github.com>
2025-11-29 22:36:44 +0300

    Create lab6_2.c

---
+++ lab6_2.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define MAX_SIZE 1000  
+
+int main() {
+    srand(time(NULL));
+    int size, k = 0, n = 0;
+    float Yk[MAX_SIZE], s_pos = 0;
+    float* ptr = Yk; // Объявление указателя
+    printf("Enter arr size: "); scanf("%d", &size);
+    if (size > MAX_SIZE) { size = MAX_SIZE; }
+    for (int i = 0; i < size; i++) {
+        Yk[i] = (rand() % 201 - 100) + (rand() % 10) / 10.0;
+        printf("%f \t", *ptr); // Указатель вместо Xn[i]
+        if (*ptr > 0) { // Указатель вместо Xn[i]
+            k++;
+            if (k == 2) { s_pos = *ptr; } // Указатель вместо Xn[i]
+            else if (k > 2) { break; }
+        }
+        if (fabsf(*ptr - roundf(*ptr)) < 0.0001f) { n++; } // Указатель вместо Xn[i]
+        ptr++; // Увеличение указателя на 1
+    }
+    ptr = Yk; //
+    if (n == 0) { printf("\nThere are no integer elements"); }
+    else {
+        printf("\nInteger elements: \t");
+        for (int i = 0; i < size; i++) {
+            if (fabsf(*ptr - roundf(*ptr)) < 0.0001f) { printf("%f \t", *ptr); } // Указатель вместо Xn[i]
+            ptr++; // Увеличение указателя на 1
+        }
+    }
+    ptr = Yk; //
+    if (s_pos != 0) {
+        printf("\nSecond positive element: %f\n", s_pos);
+        for (int i = 0; i < size; i++) {
+            if (i % 2 != 0) { printf("%f \t", s_pos); }
+            else { printf("%f \t", *ptr); }
+            ptr++; // Увеличение указателя на 1
+        } 
+    }
+    else { printf("\nThere is no second positive element, arr is same"); }
+
+    return 0;
+}
