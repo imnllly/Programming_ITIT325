65acda3a7fc97994ff3253cc120cc6ff43a0d230
imnllly <143996748+imnllly@users.noreply.github.com>
2025-12-01 12:45:57 +0300

    Update lab5_3.c

--- lab5_3.c
+++ lab5_3.c
@@ -13,13 +13,19 @@ int main() {
     printf("Enter columns and rows num: "); scanf("%d", &column);
     if (column > MAX_SIZE) { column = MAX_SIZE; }
     if (column < 4 || column == MAX_SIZE) {
-        printf("Martix is out of range"); 
+        printf("Martix is out of range");
     }
     else {
         float matrix[MAX_SIZE][MAX_SIZE];
         for (int i = 0; i < column; i++) {
             for (int j = 0; j < column; j++) {
                 matrix[i][j] = (rand() % 15) + (rand() % 10) / 10.0;
+                printf("Element [%d][%d]: %f\n", i, j, matrix[i][j]);
+            }
+        }
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < column; j++) {
+                
                 if ((j % 2 != 0) && (fabsf(matrix[i][j] - roundf(matrix[i][j])) < 0.0001f)) { num++; }
                 printf("%f\t", matrix[i][j]);
             }
@@ -29,7 +35,7 @@ int main() {
         printf("New arr:\n");
         for (int i = 0; i < column; i++) {
             for (int j = 0; j < column; j++) {
-                if (j+1 < (column - i)) { matrix[i][j] = num; }
+                if (j + 1 < (column - i)) { matrix[i][j] = num; }
                 printf("%f\t", matrix[i][j]);
             }
             printf("\n");
